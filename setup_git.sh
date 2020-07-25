3ae96e33ba1eb0d4999c2aa329caca7659aa3251
Alex White <space.monkey.delivers@gmail.com>
2020-07-26 00:51:10 +0300

    First mte test added

--- setup_git.sh
+++ setup_git.sh
@@ -15,7 +15,3 @@ cd ${ROOT_DIR}
 cd ${GCC_SRC_DIR}/riscv-binutils/
 git checkout riscv_mte-dev
 cd ${ROOT_DIR}
-
-cd ${GCC_SRC_DIR}/riscv-gdb/
-git checkout riscv_mte-dev
-cd ${ROOT_DIR}
---
+++ src/tests/testcase_1a.s
@@ -0,0 +1,20 @@
+.global main
+
+.text
+.balign 8
+main:
+    li a0, 0xAB
+    li a1, 0x80000000
+    st a0, 0(a1)
+    lt a2, 0(a1)
+    bne a2, a0, fail
+
+success:
+    li a0, 0
+    /* writing to 0x8c3 csr makes qemu to exit */
+    csrw 0x8c3, a0
+
+fail:
+    li a0, 1
+    /* writing to 0x8c3 csr makes qemu to exit */
+    csrw 0x8c3, a0
